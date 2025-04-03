0a92167e39b5bb8e00a633af00d25dfaf04053f1
BMDuar <bentoduarte13@gmail.com>
2025-04-03 17:03:57 -0300

    refinando as rotas + morgan

--- server.js
+++ server.js
@@ -1,103 +1,163 @@
 const express = require("express");
 const mysql = require("mysql2");
 const cors = require("cors");
+const morgan = require("morgan");
 
 const app = express();
 const port = 3001;
-app.use(cors());
+
+const cors_options = {
+  origin: "*",
+  methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
+  preflightContinue: false,
+  optionsSuccessStatus: 200,
+};
+app.use(cors(cors_options));
 app.use(express.json());
+app.use(morgan("dev"));
 
 // Conectar ao MySQL
 const db = mysql.createConnection({
-    host: "localhost",  
-    user: "root",       
-    password: "1234",   
-    database: "docentes"
+  host: "localhost",
+  user: "root",
+  password: "1234",
+  database: "docentes",
 });
 
 db.connect((err) => {
-    if (err) {
-        console.error("Erro ao conectar ao MySQL:", err);
-    } else {
-        console.log("Conectado ao MySQL!");
-    }
+  if (err) {
+    console.error("Erro ao conectar ao MySQL:", err);
+  } else {
+    console.log("Conectado ao MySQL!");
+  }
 });
 
 // Rota para obter todos os docentes
 app.get("/docentes", (req, res) => {
-    db.query("SELECT * FROM docentes", (err, results) => {
-        if (err) {
-            res.status(500).json({ error: err.message });
-        } else {
-            res.json(results);
-        }
-    });
+  db.query("SELECT * FROM docentes", (err, results) => {
+    if (err) {
+      return res.status(500).json({ error: err.message });
+    } else {
+      return res.status(200).json(results);
+    }
+  });
 });
 
 //Rota para procurar matrícula
 app.get("/docentes/:matricula", (req, res) => {
-    const { matricula } = req.params;
-    db.query("SELECT * FROM docentes WHERE matricula = ?", [matricula], (err, results) => {
+  const { matricula } = req.params;
+  db.query(
+    "SELECT * FROM docentes WHERE matricula = ?",
+    [matricula],
+    (err, results) => {
       if (err) {
         return res.status(500).json({ error: "Erro ao buscar docente" });
       }
       if (results.length === 0) {
         return res.status(404).json({ error: "Docente não encontrado" });
       }
-      res.json(results[0]);
-    });
-  });
-  
+      res.status(200).send(results[0]);
+    }
+  );
+});
 
 // Rota para adicionar um docente
 app.post("/docentes/criar", (req, res) => {
-    const { matricula, nome, email, dataNascimento, dataAdmissao, situacao, areaConcurso, status } = req.body;
-    const sql = "INSERT INTO docentes (matricula_doc, nome_doc, email_doc, data_nasci_doc, data_adimissao_doc, situacao_doc, area_concurso_doc, status_doc) VALUES (?, ?, ?, ?, ?, ?, ?, ?)";
-    console.log(status)
-    
+  const {
+    matricula,
+    nome,
+    email,
+    dataNascimento,
+    dataAdmissao,
+    situacao,
+    areaConcurso,
+    status,
+  } = req.body;
+  const sql =
+    "INSERT INTO docentes (matricula_doc, nome_doc, email_doc, data_nasci_doc, data_adimissao_doc, situacao_doc, area_concurso_doc, status_doc) VALUES (?, ?, ?, ?, ?, ?, ?, ?)";
+  console.log(status);
 
-    db.query(sql, [matricula, nome, email, dataNascimento, dataAdmissao, situacao, areaConcurso, status], (err, result) => {
-        if (err) {
-            console.error("Erro ao inserir no MySQL:", err.sqlMessage || err);
-            res.status(500).json({ error: err.message });
-        } else {
-            res.json({ id: result.insertId, message: "Docente adicionado com sucesso!" });
-        }
-    });
+  db.query(
+    sql,
+    [
+      matricula,
+      nome,
+      email,
+      dataNascimento,
+      dataAdmissao,
+      situacao,
+      areaConcurso,
+      status,
+    ],
+    (err, result) => {
+      if (err) {
+        console.error("Erro ao inserir no MySQL:", err.sqlMessage || err);
+        res.status(500).json({ error: err.message });
+      } else {
+        res
+          .status(200)
+          .send({
+            id: result.insertId,
+            message: "Docente adicionado com sucesso!",
+          });
+      }
+    }
+  );
 });
 
 //Rota para atualizar docentes
 app.put("/docentes/editar/:matricula", (req, res) => {
-    
-    const { matricula,nome, email, dataNascimento, dataAdmissao, situacao, areaConcurso, status } = req.body;
-    
-    console.log(status)
-    console.log(req.body)
-  
-    const sql = "UPDATE docentes SET nome_doc=?, email_doc=?, data_nasci_doc=?, data_adimissao_doc=?, situacao_doc=?, area_concurso_doc=?, status_doc=? WHERE matricula_doc=?";
-    db.query(sql, [nome, email, dataNascimento, dataAdmissao, situacao, areaConcurso,status, matricula], (err) => {
-      if (err) { 
-        console.log(err)
-        return res.status(500).json({ error: "Erro ao atualizar docente" });
-      }
-      res.json({ message: "Docente atualizado com sucesso!" });
-    });
-  });
+  const {
+    matricula,
+    nome,
+    email,
+    dataNascimento,
+    dataAdmissao,
+    situacao,
+    areaConcurso,
+    status,
+  } = req.body;
 
-  //Rota para excluir docentes
-  app.delete("/docentes/excluir/:matricula",(req, res) =>{
-    const { matricula } = req.params;
-    const sql = "DELETE from docentes where matricula_doc = ?;";
-    db.query(sql, [matricula] , (err) => {
-      if (err) { 
-        console.log(err)
+  console.log(status);
+  console.log(req.body);
+
+  const sql =
+    "UPDATE docentes SET nome_doc=?, email_doc=?, data_nasci_doc=?, data_adimissao_doc=?, situacao_doc=?, area_concurso_doc=?, status_doc=? WHERE matricula_doc=?";
+  db.query(
+    sql,
+    [
+      nome,
+      email,
+      dataNascimento,
+      dataAdmissao,
+      situacao,
+      areaConcurso,
+      status,
+      matricula,
+    ],
+    (err) => {
+      if (err) {
+        console.log(err);
         return res.status(500).json({ error: "Erro ao atualizar docente" });
       }
       res.json({ message: "Docente atualizado com sucesso!" });
-    });
-  })
-  
+    }
+  );
+});
+
+//Rota para excluir docentes
+app.delete("/docentes/excluir/:matricula", (req, res) => {
+  const { matricula } = req.params;
+  const sql = "DELETE from docentes where matricula_doc = ?";
+  db.query(sql, [matricula], (err) => {
+    if (err) {
+      console.log(err);
+      return res.status(500).json({ error: "Erro ao atualizar docente" });
+    }
+    res.status(200).json({ message: "Docente atualizado com sucesso!" });
+  });
+});
 
 app.listen(port, () => {
-    console.log(`Servidor rodando em http://localhost:${port}`);
+  console.log(`Servidor rodando em http://localhost:${port}`);
 });
